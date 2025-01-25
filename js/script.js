e528906530961b09e7fb660df66cdcd0c2fe2438
okeyski <okeyski@gmail.com>
2025-01-25 18:27:00 +0100

    update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const popularMoviesDisplay = async () => {
         
 
         img.classList.add("card-img-top");
-        img.alt = `${movie.title}`;
+        img.alt = `${movie.name}`;
 
         link.appendChild(img);
 
@@ -27,7 +27,7 @@ const popularMoviesDisplay = async () => {
 
         const cardTitle = document.createElement("h5");
         cardTitle.classList.add("card-title");
-        cardTitle.textContent = `${movie.title}`;
+        cardTitle.textContent = `${movie.name}`;
 
         const cardText = document.createElement("p");
         cardText.classList.add("card-text");
@@ -47,13 +47,57 @@ const popularMoviesDisplay = async () => {
     });
 }
 
+const popularShowsDisplay = async () => {
+    const { results } = await fetchAPIData('tv/popular')
+    console.log(results);
+    results.forEach(show => {
+        const div = document.createElement("div");
+        div.classList.add("card");
+
+        const link = document.createElement("a");
+        link.href = `tv-details.html?id=${show.id}`;
+
+        const img = document.createElement("img");
+        img.src = `${show.poster_path ? `https://image.tmdb.org/t/p/w500${show.poster_path}` : "/images/no-image.jpg"
+        }` 
+        
+
+        img.classList.add("card-img-top");
+        img.alt = `${show.name}`;
+
+        link.appendChild(img);
+
+        const div2 = document.createElement("div");
+        div2.classList.add("card-body");
+
+        const cardTitle = document.createElement("h5");
+        cardTitle.classList.add("card-title");
+        cardTitle.textContent = `${show.name}`;
+
+        const cardText = document.createElement("p");
+        cardText.classList.add("card-text");
+
+        const smallText = document.createElement("small");
+        smallText.classList.add("text-muted");
+        smallText.textContent = `Air date: ${show.first_air_date}`;
+
+        cardText.appendChild(smallText);
+        div2.appendChild(cardTitle);
+        div2.appendChild(cardText);
+
+        div.appendChild(link);
+        div.appendChild(div2);
+        
+        document.getElementById('popular-shows').appendChild(div)
+    });
+}
 
 
 
 
 
 const fetchAPIData = async (endpoint) => {
-    const API_KEY = '28c44b66e23af35eff059e4a676db902'
+    const API_KEY = '28c44b66e23af35eff059e4a676db902' //used only for development purposes
     const API_PATH = 'https://api.themoviedb.org/3/'
     const response = await fetch(`${API_PATH}${endpoint}?api_key=${API_KEY}`)
     
@@ -97,7 +141,7 @@ const init = ()=> {
         console.log('Search');            
             break;
         case '/shows.html':
-        console.log('Shows');            
+        popularShowsDisplay();            
             break;
         case '/tv-details.html':
         console.log('Details');            
